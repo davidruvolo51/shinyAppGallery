7c46ba7bb9676efd539f54b311b4bbd73b47316a
davidruvolo51 <davidruvolo51@gmail.com>
2020-11-30 22:07:48 +0100

    removed outdated app

--- NFLAttendanceRecords/external/builder.R
+++
@@ -1,8 +0,0 @@
-#-------------------------------------------------------------------------------
-# builder.R 
-# purpose: for doing the heavy lifting for generating the rest of the graphs
-# receives: map click and value
-## send: sends outputs to tab_main.R for layout and css
-#-------------------------------------------------------------------------------
-# BEGIN
-
